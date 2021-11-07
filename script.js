409aa00b45d740a933bc2fb4a160d361084ad91c
Douglas-Santana <douguinhodns@gmail.com>
2021-11-07 11:05:43 -0300

    fix: eslint

--- script.js
+++ script.js
@@ -1,95 +1,95 @@
 // Definição das cores da paleta
 const generateRandomRGB = () => {
-    const r = Math.floor(Math.random() * (255));
-    const g = Math.floor(Math.random() * (255));
-    const b = Math.floor(Math.random() * (255));
-    const rgb = `rgb(${r}, ${g}, ${b})`;
-    return rgb;
-  };
+  const r = Math.floor(Math.random() * (255));
+  const g = Math.floor(Math.random() * (255));
+  const b = Math.floor(Math.random() * (255));
+  const rgb = `rgb(${r}, ${g}, ${b})`;
+  return rgb;
+};
 
 const color = document.querySelectorAll('.color');
 function colorsPallete() {
-    color[0].style.backgroundColor = generateRandomRGB();
-    color[1].style.backgroundColor = generateRandomRGB();
-    color[2].style.backgroundColor = generateRandomRGB();
-    color[3].style.backgroundColor = generateRandomRGB();
+  color[0].style.backgroundColor = generateRandomRGB();
+  color[1].style.backgroundColor = generateRandomRGB();
+  color[2].style.backgroundColor = generateRandomRGB();
+  color[3].style.backgroundColor = generateRandomRGB();
 }
-colorsPallete()
+colorsPallete();
 
 // Botão mudar cores
-const changeColors = document.querySelector('#change-colors').addEventListener('click', () => {
-    colorsPallete()
+document.querySelector('#change-colors').addEventListener('click', () => {
+  colorsPallete();
 });
 
 // Criação do quadro
 const pixel = document.querySelector('#pixel-board');
 
 function createFrame(sizePixels = 5) {
-    for (let index = 1; index <= sizePixels; index += 1) {
-        let column = document.createElement('div');
-        pixel.appendChild(column);
-        for (let index2 = 1; index2 <= sizePixels; index2 += 1) {
-            let eachPixel = document.createElement('div');
-            column.appendChild(eachPixel);
-            eachPixel.className = 'pixel';
-        }
+  for (let index = 1; index <= sizePixels; index += 1) {
+    const column = document.createElement('div');
+    pixel.appendChild(column);
+    for (let index2 = 1; index2 <= sizePixels; index2 += 1) {
+      const eachPixel = document.createElement('div');
+      column.appendChild(eachPixel);
+      eachPixel.className = 'pixel';
     }
+  }
 }
-createFrame()
+createFrame();
 
 // Definição do tamanho do quadro pelo usuario
 const btnSizePixels = document.querySelector('#set-pixels');
 const inputNumber = document.querySelector('#input-number');
 
 btnSizePixels.addEventListener('click', () => {
-    sizePixels = inputNumber.value;
-    if (sizePixels === '') alert('Board inválido!');
-    if (sizePixels < 5) {
-        pixel.innerHTML = '';
-        return createFrame(5);
-    }
-    if (sizePixels > 50) {
-        pixel.innerHTML = '';
-        return createFrame(50);
-    }
+  const sizePixels = inputNumber.value;
+  if (sizePixels === '') alert('Board inválido!');
+  if (sizePixels < 5) {
     pixel.innerHTML = '';
-    return createFrame(sizePixels);
-})
+    return createFrame(5);
+  }
+  if (sizePixels > 50) {
+    pixel.innerHTML = '';
+    return createFrame(50);
+  }
+  pixel.innerHTML = '';
+  return createFrame(sizePixels);
+});
 
 // Define a primeira cor da paleta como padrão
 color[0].className += ' selected';
 
 // Seleciona a classe "selected" onde for clicado
-function changeClass () {
-    const elementColors = document.querySelector('#color-palette');
-    elementColors.addEventListener('click', (e) => {
+function changeClass() {
+  const elementColors = document.querySelector('#color-palette');
+  elementColors.addEventListener('click', (e) => {
     const colorElement = e.target;
     const selectElement = document.querySelector('.selected');
     selectElement.classList.remove('selected');
     colorElement.classList.add('selected');
-    })
+  });
 }
-changeClass()
+changeClass();
 
 // Define a cor do pixel clicado com a cor da paleta selecionada
 function clickColor() {
-    document.querySelector('#pixel-board').addEventListener('click', (e) => {
-        const eachPixel = e.target;
-        if (eachPixel.className === 'pixel') {
-            const colorSelected = document.querySelector('.selected').style.backgroundColor;
-            eachPixel.style.backgroundColor = colorSelected;
-        }
-    })
+  document.querySelector('#pixel-board').addEventListener('click', (e) => {
+    const eachPixel = e.target;
+    if (eachPixel.className === 'pixel') {
+      const colorSelected = document.querySelector('.selected').style.backgroundColor;
+      eachPixel.style.backgroundColor = colorSelected;
+    }
+  });
 }
-clickColor()
+clickColor();
 
 // botao de limpar tudo
 function btnClear() {
-    document.querySelector('#erase-all-btn').addEventListener('click', () => {
-        const pixels = document.querySelectorAll('.pixel');
-        for (let index = 0; index < pixels.length; index ++) {
-            pixels[index].style.backgroundColor = 'white';
-        }
-    });
+  document.querySelector('#erase-all-btn').addEventListener('click', () => {
+    const pixels = document.querySelectorAll('.pixel');
+    for (let index = 0; index < pixels.length; index += 1) {
+      pixels[index].style.backgroundColor = 'white';
+    }
+  });
 }
-btnClear()
+btnClear();
